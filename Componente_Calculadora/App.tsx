e7170994ab3271bb5cb0e9e567a5bdbe8dbd404c
Paola Deras <paola_deras@unitec.edu>
2025-01-30 13:51:33 -0600

    correccion de errores

--- Componente_Calculadora/App.tsx
+++ Componente_Calculadora/App.tsx
@@ -1,19 +1,39 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-import Calculadora from "./componentes/calculadora";
+import CalculatorComponent from "./componentes/calculadora";
 
 export default function App() {
   return (
     <View style={styles.contenedor}>
       <Text style={styles.titulo}>Calculadora</Text>
-      <Calculadora num1={10} num2={5} operacion={1} />
-      <Calculadora num1={10} num2={5} operacion={2} />
-      <Calculadora num1={10} num2={5} operacion={3} />
-      <Calculadora num1={10} num2={5} operacion={4} />
-      <Calculadora num1={10} num2={0} operacion={4} /> 
+      {}
+      <CalculatorComponent a={10} b={5} operation={1} /> {}
+      <CalculatorComponent a={10} b={5} operation={2} /> {}
+      <CalculatorComponent a={10} b={5} operation={3} /> {}
+      <CalculatorComponent a={10} b={5} operation={4} /> {}
+      <CalculatorComponent a={10} b={0} operation={4} /> {}
     </View>
   );
 }
+
+const stylesCalculadora = StyleSheet.create({
+  contenedor: {
+    padding: 10,
+    margin: 5,
+    backgroundColor: "#D8BFD8", 
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  texto: {
+    fontSize: 18,
+    color: "#4B0082", 
+  },
+  resultado: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#6A0DAD", 
+  },
+});
 
 const styles = StyleSheet.create({
   contenedor: {
--- Componente_Calculadora/componentes/Calculadora.tsx
+++ Componente_Calculadora/componentes/Calculadora.tsx
@@ -1,63 +1,69 @@
 import React from "react";
 import {View, Text, StyleSheet} from "react-native";
 
+
+type operacion = 1 | 2 | 3 | 4;
+
 interface Props {
-    num1: number;
-    num2: number;
-    operacion: number;
+  a: number;
+  b: number;
+  operation: operacion;
 }
 
-const Calculadora: React.FC<Props> = ({ num1, num2, operacion}) => {
-    let resultado: string | number;
-    let operacionTexto: string;
 
-    switch (operacion) {
-        case 1:
-          resultado = num1 + num2;
-          operacionTexto = "Suma";
-          break;
-        case 2:
-          resultado = num1 - num2;
-          operacionTexto = "Resta";
-          break;
-        case 3:
-          resultado = num1 * num2;
-          operacionTexto = "Multiplicación";
-          break;
-        case 4:
-          resultado = num2 !== 0 ? num1 / num2 : "Error: No se puede dividir entre 0";
-          operacionTexto = "División";
-          break;
-        default:
-          resultado = "Operación no válida";
-          operacionTexto = "Desconocida";
-      }
-      return (
-        <View style={stylesCalculadora.contenedor}>
-          <Text style={stylesCalculadora.texto}>{operacionTexto}: {num1} y {num2}</Text>
-          <Text style={stylesCalculadora.resultado}>Resultado: {resultado}</Text>
-        </View>
-      );
-    };
-    
+const CalculatorComponent: React.FC<Props> = ({ a, b, operation }) => {
+  let resultado: string | number;
+  let operationTexto: string;
 
-    const stylesCalculadora = StyleSheet.create({
-        contenedor: {
-          padding: 10,
-          margin: 5,
-          backgroundColor: "#D8BFD8", 
-          borderRadius: 10,
-          alignItems: "center",
-        },
-        texto: {
-          fontSize: 18,
-          color: "#4B0082", 
-        },
-        resultado: {
-          fontSize: 20,
-          fontWeight: "bold",
-          color: "#6A0DAD", 
-        },
-      });
-      
-      export default Calculadora;
+  switch (operation) {
+    case 1:
+      resultado = a + b;
+      operationTexto = "Suma";
+      break;
+    case 2:
+      resultado = a - b; 
+      operationTexto = "Resta";
+      break;
+    case 3:
+      resultado = a * b; 
+      operationTexto = "Multiplicación";
+      break;
+    case 4:
+   
+      resultado = b !== 0 ? a / b : "Error: No se puede dividir entre 0";
+      operationTexto = "División";
+      break;
+    default:
+      resultado = "Operación no válida"; 
+      operationTexto = "Desconocida";
+  }
+
+  return (
+    <View style={stylesCalculadora.contenedor}>
+      <Text style={stylesCalculadora.texto}>
+        {operationTexto}: {a} y {b}
+      </Text>
+      <Text style={stylesCalculadora.resultado}>Resultado: {resultado}</Text>
+    </View>
+  );
+};
+const stylesCalculadora = StyleSheet.create({
+    contenedor: {
+      padding: 10,
+      margin: 5,
+      backgroundColor: "#D8BFD8",
+      borderRadius: 10,
+      alignItems: "center",
+    },
+    texto: {
+      fontSize: 18,
+      color: "#4B0082",
+    },
+    resultado: {
+      fontSize: 20,
+      fontWeight: "bold",
+      color: "#6A0DAD",
+    },
+  });
+  
+  export default CalculatorComponent;
